a18546a0e5c6d95311b70cefe09370c5a8fe4f73
SeoJeongmu <wsxcfthn@gmail.com>
2023-08-11 14:57:24 +0900

    인트로 페이지, 목차 페이지 구분 형성

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from datetime import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from page import intro as it
 from page import se_1 as s1
 from page import se_2 as s2
 from page import se_3 as s3
@@ -10,10 +11,13 @@ from page import page_env as penv
 from page import manual_page as man
 
 
-item = st.sidebar.selectbox('항목을 골라요.',  ['선택1', '가상환경','streamlit manual','선택3',] )
+item = st.sidebar.selectbox('항목을 골라요.', ['intro','목차', '가상환경','streamlit manual','선택3',] )
 
 
-if item == '선택1':
+
+if item == 'intro':
+    it.app()   
+elif item == '목차1':
     s1.app()   
     # 데이터프레임
     # 그래프 맵
---
+++ page/intro.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+
+def app():
+    # 제목에 띄어쓰기와 들여쓰기를 적용한 스타일
+    st.markdown('<span style="padding-left: 220px; white-space:pre; font-size: 40px; font-weight: bold; color: #1e90ff">2조 퓨처 파이썬</h1>', unsafe_allow_html=True)
+
+    st.markdown('<span style="padding-left: 120px; white-space:pre; font-size: 50px; font-weight: bold;">제주도 날씨 변화에 따른</span>', unsafe_allow_html=True)
+    st.markdown('<span style="padding-left: 0px; white-space:pre; font-size: 50px; font-weight: bold;">업종별 매출의 패턴변화 (2018-2020)</span>', unsafe_allow_html=True)
+    
+    mem = pd.DataFrame({
+        '조원': ['조장', '조원 1', '조원 2'],
+        '이름': ['서정무', '김기인', '김영훈']
+    }).set_index('조원')
+
+
+    # 표에 스타일 적용하여 텍스트 크기 조절
+    st.write('<style>th, td {font-size: 15px;}</style>', unsafe_allow_html=True)
+    st.table(mem)
+    
