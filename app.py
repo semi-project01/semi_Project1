1d980cdd9cf6365cfea10ac4178d764776fe0e62
SeoJeongmu <wsxcfthn@gmail.com>
2023-08-11 10:56:50 +0900

    app.py 초기화 , 선택1 페이지 수정중

--- app.py
+++ app.py
@@ -8,15 +8,6 @@ from page import se_2 as s2
 from page import se_3 as s3
 
 
-todays = dt.today()
-tod = todays.strftime(' %Y년 %m월 %d일')
-st.title(f'{tod}')
-st.header(f'개인적인 복습입니다. 가상환경 구축하기')
-
-st.subheader('오늘은 세미프로젝트 진행전 작업구조 만들기입니다.')
-
-st.write('만들어봅시다.')
-
 item = st.sidebar.selectbox('항목을 골라요.',  ['선택1', '선택2','선택3'] )
 
 
--- page/se_1.py
+++ page/se_1.py
@@ -1,8 +1,24 @@
 import streamlit as st
+import pandas as pd
 from utils import se_1_mo as sm1
 
 
 def app():
-    st.write('데이터프레임 및 맵플롯 구간입니다.')
-    df = sm1.get_movie()
-    st.dataframe(df)
+    st.title("2조 :blue[퓨처 파이썬]")
+
+    mem = pd.DataFrame({
+        'index': ['조장', '조원 1', '조원 2'],
+        'column': ['서정무', '김기인', '김영훈']
+    })
+
+    st.write(mem)
+
+    st.header(f'주제: 제주도 날씨 변화에 따른 업종별 매출의 패턴변화 (2018-2020)')
+
+
+    st.subheader('I N D E X')
+    
+    st.write('1. 가상환경 구축 매뉴얼')
+    st.write('2. streamlit 매뉴얼')
+    st.write('3. 프로젝트 diagram')
+    st.write('4. 처리 및 분석한 내용')
