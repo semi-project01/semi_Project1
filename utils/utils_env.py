266ec46ba87383e3f0da2042c408e1ddbb065726
kimgiin <goldgiin@naver.com>
2023-08-11 13:45:53 +0900

    가상환경 2차 untils의 오타 및 체크박스 수정

--- utils/utils_env.py
+++ utils/utils_env.py
@@ -13,16 +13,15 @@ def desc():
             ### 2. conda env list 를 입력합니다.
                  이것은 현재 아나콘에 있는 가상환경 목록을 보여줍니다.
             ''')
-    if st.checkbox('이미지 보기'):
-        st.image('image\image01.png', use_column_width = True)
-    else:
-        pass
+
+    st.image('image\image01.png', use_column_width = True)
+
     st.write('''
-            ### 3. conda create -n prjsmi python=3.9 ipython numpy pandas matplotlib folium
+            ### 3. conda create -n semiproject python=3.9 ipython numpy pandas matplotlib folium
                             
-                prjsmi이란 가상환경 이름으로 파이썬 버전을 정해주고
+                semiproject이란 가상환경 이름으로 파이썬 버전을 정해주고
             
-                설치하고자 하는 라이브러리를 적어줍니다.
+                설치하고자 하는 라이브러리들를 적어줍니다.
             
                 이 때 streamlit은 아나콘다의 repository에 없어서 에러가 발생하므로 나중에 설치해 줍니다.
             ''')
@@ -30,9 +29,9 @@ def desc():
     st.write(''''
             ### 4. 설치 완료
             
-            conda activate semiproject : semiproject 가상환경을 실행한다.
+            conda activate semiproject : semiproject 가상환경을 실행합니다.
                 
-    codna deactivate : 가상환경을 나온다.
+    codna deactivate : 가상환경을 나옵니다.
             ''')
     st.image('image\image03.png')
     st.write('''
