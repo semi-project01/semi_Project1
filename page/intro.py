858bb09d7b5d2c8b07af020a31a5012979a3e7d7
SeoJeongmu <wsxcfthn@gmail.com>
2023-08-11 15:06:08 +0900

    내용 수정

--- page/intro.py
+++ page/intro.py
@@ -5,10 +5,11 @@ import pandas as pd
 def app():
     # 제목에 띄어쓰기와 들여쓰기를 적용한 스타일
     st.markdown('<span style="padding-left: 220px; white-space:pre; font-size: 40px; font-weight: bold; color: #1e90ff">2조 퓨처 파이썬</h1>', unsafe_allow_html=True)
-
+    st.markdown('<br>',unsafe_allow_html=True)
     st.markdown('<span style="padding-left: 120px; white-space:pre; font-size: 50px; font-weight: bold;">제주도 날씨 변화에 따른</span>', unsafe_allow_html=True)
     st.markdown('<span style="padding-left: 0px; white-space:pre; font-size: 50px; font-weight: bold;">업종별 매출의 패턴변화 (2018-2020)</span>', unsafe_allow_html=True)
-    
+    st.markdown("<br>",unsafe_allow_html=True)
+
     mem = pd.DataFrame({
         '조원': ['조장', '조원 1', '조원 2'],
         '이름': ['서정무', '김기인', '김영훈']
--- page/se_1.py
+++ page/se_1.py
@@ -11,9 +11,9 @@ def app():
         #'<span style="padding-left: 200px;; font-size: 70px; font-weight: bold; color: #1e90ff">•</span>'
         '<span style="padding-left: 150px; white-space:pre; font-size: 70px; font-weight: bold; color: #1e90ff">•    I N D E X    •</span>', unsafe_allow_html=True)
     st.markdown('----')
-    st.markdown('<p style="font-size: 30px; font-weight: 650;">1. 가상환경 구축 매뉴얼</p>', unsafe_allow_html=True)
-    st.markdown('<p style="font-size: 30px; font-weight: 650;">2. Streamlit 사용 메뉴얼</p>', unsafe_allow_html=True)
-    st.markdown('<p style="font-size: 30px; font-weight: 650;">3. 프로젝트 diagram</p>', unsafe_allow_html=True)
-    st.markdown('<p style="font-size: 30px; font-weight: 650;">4. 처리 및 분석한 내용</p>', unsafe_allow_html=True)
+    st.markdown('<p style="padding-left: 170px; font-size: 30px; font-weight: 650;">1. 가상환경 구축 매뉴얼</p>', unsafe_allow_html=True)
+    st.markdown('<p style="padding-left: 170px; font-size: 30px; font-weight: 650;">2. Streamlit 사용 메뉴얼</p>', unsafe_allow_html=True)
+    st.markdown('<p style="padding-left: 170px; font-size: 30px; font-weight: 650;">3. 프로젝트 diagram</p>', unsafe_allow_html=True)
+    st.markdown('<p style="padding-left: 170px; font-size: 30px; font-weight: 650;">4. 처리 및 분석한 내용</p>', unsafe_allow_html=True)
 
     
