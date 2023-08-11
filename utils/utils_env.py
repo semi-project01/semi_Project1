0775900b6b2b8fe6bf79027f9a61982a7986af23
SeoJeongmu <wsxcfthn@gmail.com>
2023-08-11 16:25:27 +0900

    가상공간 형성 내용 수정

--- utils/utils_env.py
+++ utils/utils_env.py
@@ -7,38 +7,66 @@ Created on Thu Aug 10 13:38:30 2023
 
 import streamlit as st
 
-def desc():
+def desc():    
     st.write('''
-            ### 1. Powershell Prompt를 실행 합니다.
-            ### 2. conda env list 를 입력합니다.
-                 이것은 현재 아나콘에 있는 가상환경 목록을 보여줍니다.
+            ### 1. 먼저 Powershell Prompt를 실행 합니다.
+            ### 2. 현재 가지고 있는 가상환경을 확인합니다.
+                
+                -명령어 : conda env list 를 입력합니다.
+                -현재 아나콘에 있는 가상환경 목록을 보여줍니다.
             ''')
-
+    st.write('입력 후 아래와 같이 확인됩니다.')   
     st.image('image\image01.png', use_column_width = True)
 
     st.write('''
-            ### 3. conda create -n semiproject python=3.9 ipython numpy pandas matplotlib folium
-                            
-                semiproject이란 가상환경 이름으로 파이썬 버전을 정해주고
-            
-                설치하고자 하는 라이브러리들를 적어줍니다.
-            
-                이 때 streamlit은 아나콘다의 repository에 없어서 에러가 발생하므로 나중에 설치해 줍니다.
-            ''')
-    st.image('image\image02.png') #y누르는 부분
-    st.write('''
-            ### 4. 설치 완료
-            
-            conda activate semiproject : semiproject 가상환경을 실행합니다.
-                
-    codna deactivate : 가상환경을 나옵니다.
-            ''')
-    st.image('image\image03.png')
-    st.write('''
-             ### 5. 제대로 설치가 되어있는 확인하는 단계
-             
-                 pyhthon --version : 파이선의 버전을 확인하는 명령어 입니다.
-             
+            ### 3.새 가상환경 만들기(파이썬 버전 및 라이브러리)
              ''')
+    st.code('''
+            '가상공간' 형성 명령어 예시
+            conda create -n semiproject python=3.9 ipython numpy pandas matplotlib folium
+
+            #conda create -n semiproject 
+            'semiproject'라는 이름의 가상공간을 만듭니다.
+
+            #python=3.9 ipython
+            '3.9버전 파이썬'으로 설치를 진행합니다.
+
+            #numpy pandas matplotlib folium
+            필요한 라이브러리 4가지를 함께 설치를 진행합니다.
+            (numpy, pandas, matplotlib, folium)
+            ''')
+    st.write('''semiproject이란 가상환경 이름으로 파이썬 버전을 정해주고, 설치하고자 하는 라이브러리들를 마지막 뒤에 적어줍니다.
+             ''')
+    st.markdown('----')
+    st.markdown('<span style="font-weight: bold; color: gray"> * 이 때 streamlit은 아나콘다의 repository에 없어서 에러가 발생하는 경우 별도 설치해 줍니다.</span>', unsafe_allow_html=True)
+    #st.write('''이 때 streamlit은 아나콘다의 repository에 없어서 에러가 발생하는 경우 별도 설치해 줍니다.''')
+
+    st.markdown('<span style="padding-left: 220px; white-space:pre; font-size: 20px; font-weight: bold; color: red">pip install streamlit</span>', unsafe_allow_html=True)
+    st.markdown('----')
+    st.image('image\image02.png') #y누르는 부분
+    st.markdown("procceed 질문이 나오면 '[y]'를 눌러주세요")
+    st.markdown(" 설치를 끝마치면 가상환경 생성이 되었습니다. ")
+    st.write('''
+            ### 4. 가상황경 활성화 및 비활성화 
+            ''')
+    st.code('''
+            - 'semiproject 가상환경'을 실행합니다.
+            conda activate semiproject
+            ''')
+                
+    st.code('''
+            - '가상환경'을 나옵니다.
+            codna deactivate 
+                        ''')
+    st.image('image\image03.png',use_column_width = True)
+    # 여기 이미지 활성화 비활성화 이미지 변경하는게 좋을 것 같음 
+
+    st.write('''
+             ### 5. 제대로 설치가 되어있는 재확인
+             ''')
+    st.code('''
+            - '파이선의 버전'을 확인하는 명령어 입니다.
+            pyhthon --version 
+            ''')
     st.image('image\image04.png')
     
