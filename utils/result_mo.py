453c829a87bd507fc73eb8a8cf44a1c37acbdfd3
SeoJeongmu <wsxcfthn@gmail.com>
2023-08-17 13:36:10 +0900

    no message

--- utils/result_mo.py
+++ utils/result_mo.py
@@ -61,3 +61,22 @@ def result():
     for industry in industry_list:
         st.subheader(f'{industry} - 계절별 평균 매출액 데이터')
         plot_for_industry_t(data, industry)
+
+
+
+
+    st.header('[최종 결론]')
+    st.subheader('여름 : 제주도 방문객에게 추천하는 업종')
+    st.image('./image/result01.png', use_column_width=True)
+    st.write('> 여름에는 숙박, 요식업, 운송업이 매출이 높습니다')
+
+    st.subheader('가을 : 제주도 방문객에게 추천하는 업종')
+    st.image('./image/result02.png', use_column_width=True)
+    st.write('> 가을에는 골프장, 스포츠, 오락관광 및 여가, 소매업이 매출이 높습니다')
+
+    st.write('1. 계절에 따른 업종별 평균 매출 변화를 분석하여 날씨의 유의미한 영향을 확인하여 각 업종별로 가장 높은 매출을 기록한 계절을 확인 가능')
+
+    st.write('2. 분석 결과물들을 통해 수많은 관광객들의 각자의 목적에 맞는 여행 시기를 설정하는데 도움을 줄 수 있음')
+
+    st.write('3. 이외의 여행 목적에 따른 지역추천에 대한 분석은 데이터수집에 어려움이 있어 문제해결에 어려움이 있음')
+
